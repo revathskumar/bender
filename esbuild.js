8a0f12cd8f73c06d710a70170724f178331cde34
Revath S Kumar <rsk@revathskumar.com>
2026-06-04 20:11:13 +0200

    chore(build): target updated to firefox128 and gjs 1.81.3+

--- esbuild.js
+++ esbuild.js
@@ -4,12 +4,14 @@ await build({
   entryPoints: ["src/index.ts"],
   outdir: "dist/",
   bundle: true,
+  // Find SpiderMonkey version at https://gitlab.gnome.org/GNOME/gjs/raw/HEAD/NEWS
   // target: "firefox60", // Since GJS 1.53.90
   // target: "firefox68", // Since GJS 1.63.90
   // target: "firefox78", // Since GJS 1.65.90
   // target: "firefox91", // Since GJS 1.71.1
   // target: "firefox102", // Since GJS 1.73.2
-  target: "firefox115", // Since GJS 1.77.2
+  // target: "firefox115", // Since GJS 1.77.2
+  target: "firefox128", // Since GJS  1.81.2
   format: "esm",
   external: ["gi://*"],
 });
